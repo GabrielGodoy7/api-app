09c9f4b1b167d0ef63698c1c1e2f4177885b0941
GabrielGodoy7 <142518644+GabrielGodoy7@users.noreply.github.com>
2023-09-21 22:19:59 -0300

    Initial commit

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from '../Models/Usuario.models';
+import { HttpClient } from '@angular/common/http';
+import { EMPTY, Observable, catchError, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsuarioService {
+
+  public listaUsuario: Usuario[] = [];
+  private url = 'http://localhost:3000/usuarios'
+
+  constructor(private http: HttpClient) { }
+
+
+  public getAll(): Observable<Usuario[]>{
+    return this.http.get<Usuario[]>(this.url).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibeErro(erro))
+    );
+  }
+
+  public getOne(id: number): Observable<Usuario> { // Correção da tipagem
+    return this.http.get<Usuario>(`${this.url}/${id}`) // Use crases para interpolar a variável id
+      .pipe(
+        catchError(erro => this.exibeErro(erro))
+      );
+  }
+  
+
+  exibeErro(erro: any):Observable<any>
+  {
+    console.log(erro);
+    alert('Não foi possível atender a sua solicitação!');
+    return EMPTY;
+  }
+
+  salvar(usuario: Usuario):Observable<Usuario>{
+    return this.http.post<Usuario>(this.url, usuario).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibeErro(erro))
+    );
+  }
+}
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../services/usuario.service';
+import { Usuario } from '../Models/Usuario.models';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+
+
+export class Tab1Page implements OnInit {
+
+listaUsuarios: Usuario[] = [];
+usuario?: Usuario;
+id = 0;
+
+  constructor(private userService: UsuarioService) {
+    
+  }
+
+  buscarUsuarios(){
+    this.userService.getAll().subscribe(retorno =>{
+      // "as Usuario[]" tenta converter o retorno
+      this.listaUsuarios = retorno as Usuario[];
+      console.log(this.listaUsuarios); // Limpar o Usuario
+      this.usuario = undefined;
+    });
+  }
+
+  buscarPorID(){
+    this.userService.getOne(this.id).subscribe(retorno =>{
+      console.log(retorno);
+      this.usuario = retorno as unknown as Usuario;
+      this.listaUsuarios = []; // Limpar a lista
+    });
+  }
+  ngOnInit(): void{
+    this.buscarUsuarios();
+  }
+
+}
